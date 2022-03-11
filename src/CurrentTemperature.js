0410e7314f9cdcb1cf385c9bd0feabeedd9092c0
DIANA-DIANA7 <dianasmejia21@gmail.com>
2022-03-10 16:59:02 -0800

    moved weather details into current Temperature component
    applied unit conversion to work on high, low temps and wind speed.

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -14,27 +14,60 @@ if (unit === "fahrenheit" ){
     return (
       <div className="currentTemperature">
         {Math.round(props.fahrenheit)}
-        <small id="fah"> F°</small>
-        |
+        <small id="fah"> F°</small>|
         <small id="cel">
-          <a href="/" onClick={convertToCelsius}> C°</a>
+          <a href="/" onClick={convertToCelsius}>
+            {" "}
+            C°
+          </a>
         </small>
+        <ul>
+          <li>
+            High: <span id="high">{Math.round(props.high)}</span>°
+          </li>
+          <li>
+            Low: <span id="low">{Math.round(props.low)}</span>°
+          </li>
+          <li>
+            Wind: <span id="wind">{Math.round(props.wind)}</span>mph
+          </li>
+          <li>
+            Humidity: <span id="humid">{props.humid}</span>%
+          </li>
+        </ul>
       </div>
     );
 } else { 
     let celsiusTemp=(props.fahrenheit-32)* 5/9;
+    let highTemp=(props.high-32)* 5/9;
+    let lowTemp=(props.low-32)* 5/9;
+    let windSpeed = (props.wind) * 1.609;
+
+    
     return (
-  <div className="currentTemperature">
-    {Math.round(celsiusTemp)}
-    <small id="fah">  
-    <a href="/" onClick={convertToFahrenheit}>
-        F°
-    </a>
-    </small>|
-    <small id="cel">
-      C°
-    </small>
-  </div>
-);
+      <div className="currentTemperature">
+        {Math.round(celsiusTemp)}
+        <small id="fah">
+          <a href="/" onClick={convertToFahrenheit}>
+            F°
+          </a>
+        </small>
+        |<small id="cel">C°</small>
+        <ul>
+          <li>
+            High: <span id="high">{Math.round(highTemp)}</span>°
+          </li>
+          <li>
+            Low: <span id="low">{Math.round(lowTemp)}</span>°
+          </li>
+          <li>
+            Wind: <span id="wind">{Math.round(windSpeed)}</span>km/h
+          </li>
+          <li>
+            Humidity: <span id="humid">{props.humid}</span>%
+          </li>
+        </ul>
+      </div>
+    );
 }
 }
--- src/Weather.js
+++ src/Weather.js
@@ -7,30 +7,21 @@ export default function Weather(props) {
     <div>
       <h1 id="searched-city">{props.details.city}</h1>
       <h2>
-        
-        <FormatDate  />
+        <FormatDate />
       </h2>
-      <Icons code={props.details.icon}/>
+      <Icons code={props.details.icon} />
 
       <span className="description" id="weather-description">
         <br />
         {props.details.description}
       </span>
-      <CurrentTemperature fahrenheit={props.details.temperature} />
-      <ul>
-        <li>
-          High: <span id="high">{Math.round(props.details.high)}</span>°
-        </li>
-        <li>
-          Low: <span id="low">{Math.round(props.details.low)}</span>°
-        </li>
-        <li>
-          Wind: <span id="wind">{Math.round(props.details.wind)}</span>mph
-        </li>
-        <li>
-          Humidity: <span id="humid">{props.details.humidity}</span>%
-        </li>
-      </ul>
+      <CurrentTemperature
+        fahrenheit={props.details.temperature}
+        high={props.details.high}
+        low={props.details.low}
+        wind={props.details.wind}
+        humid={props.details.humidity}
+      />
     </div>
   );
 }
